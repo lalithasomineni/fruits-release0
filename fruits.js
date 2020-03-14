25e1ec3770eed8d079075a435e536e57a00098c2
lalli <lsomineni@gmail.com>
2020-03-14 17:54:04 +0530

    release 0

---
+++ fruits.js
@@ -0,0 +1,6 @@
+var fruits = ["apple","banana","cherries","grapes","pears","strawberry"];
+for(i=0;i<6;i++)
+{
+   console.log(fruits[i]);
+}
+   console.log("the number of fruits is"+ fruits.length);
